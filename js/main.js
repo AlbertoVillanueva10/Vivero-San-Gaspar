5313afb1eaab3a6c34533e081bb793f38445085d
Alberto <betoangel100@gmail.com>
2025-02-24 18:34:11 -0800

    adding styles to img logo, and adding hamburguer menu fixed, it is still missing some details

--- js/main.js
+++ js/main.js
@@ -1,15 +1,18 @@
 const menuListHam = document.querySelector('.container__menu-ham')
 const openMenuHam = document.querySelector("#btn__menu-ham--open")
 const closeMenuHam = document.querySelector('.btn__menu-ham--close')
+const bodyColor = document.querySelector('body')
 
 openMenuHam.addEventListener('click', () => {
     menuListHam.classList.toggle('open')
     openMenuHam.classList.toggle('close')
+    bodyColor.classList.toggle('opacity')
 })
 
 closeMenuHam.addEventListener('click',() => {
     openMenuHam.classList.remove('close')
     menuListHam.classList.remove('open')
+    bodyColor.classList.remove('opacity')
 })
 
 // ** Asegurando uso de dispositivos mobiles
@@ -17,9 +20,11 @@ closeMenuHam.addEventListener('click',() => {
 openMenuHam.addEventListener('touchstart', () => {
     menuListHam.classList.toggle('open')
     openMenuHam.classList.toggle('close')
+    bodyColor.classList.toggle('opacity')
 })
 
 closeMenuHam.addEventListener('touchstart',() => {
     openMenuHam.classList.remove('close')
     menuListHam.classList.remove('open')
+    bodyColor.classList.remove('opacity')
 })
