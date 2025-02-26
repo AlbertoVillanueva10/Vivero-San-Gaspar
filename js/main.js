99f5869846e852e01303a6ddeafa732b8cc4525c
Alberto <betoangel100@gmail.com>
2025-02-25 19:49:42 -0800

    giving a blur background when btn hambuguers is actived, and also hero section done for now in mobile

--- js/main.js
+++ js/main.js
@@ -1,18 +1,18 @@
 const menuListHam = document.querySelector('.container__menu-ham')
 const openMenuHam = document.querySelector("#btn__menu-ham--open")
 const closeMenuHam = document.querySelector('.btn__menu-ham--close')
-const bodyColor = document.querySelector('body')
+const blurBackground = document.querySelector('.overlay')
 
 openMenuHam.addEventListener('click', () => {
     menuListHam.classList.toggle('open')
     openMenuHam.classList.toggle('close')
-    bodyColor.classList.toggle('opacity')
+    blurBackground.classList.toggle('visible')
 })
 
 closeMenuHam.addEventListener('click',() => {
     openMenuHam.classList.remove('close')
     menuListHam.classList.remove('open')
-    bodyColor.classList.remove('opacity')
+    blurBackground.classList.remove('visible')
 })
 
 // ** Asegurando uso de dispositivos mobiles
@@ -20,11 +20,11 @@ closeMenuHam.addEventListener('click',() => {
 openMenuHam.addEventListener('touchstart', () => {
     menuListHam.classList.toggle('open')
     openMenuHam.classList.toggle('close')
-    bodyColor.classList.toggle('opacity')
+    blurBackground.classList.toggle('visible')
 })
 
 closeMenuHam.addEventListener('touchstart',() => {
     openMenuHam.classList.remove('close')
     menuListHam.classList.remove('open')
-    bodyColor.classList.remove('opacity')
+    blurBackground.classList.remove('visible')
 })
