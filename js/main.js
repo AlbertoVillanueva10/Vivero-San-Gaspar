e566abb32073191cd3b33971248ade7e71f6487e
Alberto <betoangel100@gmail.com>
2025-02-26 17:43:52 -0800

    done menu hamburguer after a link is selected

--- js/main.js
+++ js/main.js
@@ -2,6 +2,8 @@ const menuListHam = document.querySelector('.container__menu-ham')
 const openMenuHam = document.querySelector("#btn__menu-ham--open")
 const closeMenuHam = document.querySelector('.btn__menu-ham--close')
 const blurBackground = document.querySelector('.overlay')
+const anyLinkMenuHamSelected = document.querySelectorAll('#menu__ham-list a')
+
 
 openMenuHam.addEventListener('click', () => {
     menuListHam.classList.toggle('open')
@@ -27,4 +29,22 @@ closeMenuHam.addEventListener('touchstart',() => {
     openMenuHam.classList.remove('close')
     menuListHam.classList.remove('open')
     blurBackground.classList.remove('visible')
+})
+
+//** Al presionar el menu hamburguesa cerrar el menu */
+
+anyLinkMenuHamSelected.forEach(link => {
+    link.addEventListener('click', () => {
+        menuListHam.classList.toggle('open')
+        blurBackground.classList.toggle('visible')
+        openMenuHam.classList.toggle('open')
+    })
+})
+
+anyLinkMenuHamSelected.forEach(link => {
+    link.addEventListener('touchstart', () => {
+        menuListHam.classList.toggle('open')
+        blurBackground.classList.toggle('visible')
+        openMenuHam.classList.toggle('open')
+    })
 })
