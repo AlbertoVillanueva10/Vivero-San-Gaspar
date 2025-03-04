6a31f613c6725823f0007528b185f66c09d91e61
Alberto <betoangel100@gmail.com>
2025-03-03 20:11:40 -0800

    fixing mobile contact container center, js mobile gestures are missing

--- js/main.js
+++ js/main.js
@@ -1,50 +1,65 @@
-const menuListHam = document.querySelector('.container__menu-ham')
-const openMenuHam = document.querySelector("#btn__menu-ham--open")
-const closeMenuHam = document.querySelector('.btn__menu-ham--close')
+const containerMenuHam = document.querySelector('.container__menu-ham')
+const btnOpenMenuHam = document.querySelector("#btn__menu-ham--open")
+const btnXCloseMenuHam = document.querySelector('.btn__menu-ham--close')
 const blurBackground = document.querySelector('.overlay')
-const anyLinkMenuHamSelected = document.querySelectorAll('#menu__ham-list a')
+const anyLinkMenuHamSelected = document.querySelectorAll('#menu__ham-list li a')
 
 
-openMenuHam.addEventListener('click', () => {
-    menuListHam.classList.toggle('open')
-    openMenuHam.classList.toggle('close')
+// ** Al presionar el boton hamburguesa agrega las siguientes clases 
+btnOpenMenuHam.addEventListener('click', () => {
+    // ** Abre el menu hamburguesa
+    containerMenuHam.classList.toggle('open')
+    // ** Agrega el efecto borroso al resto de la pantalla, fuera del menu
     blurBackground.classList.toggle('visible')
 })
 
-closeMenuHam.addEventListener('click',() => {
-    openMenuHam.classList.remove('close')
-    menuListHam.classList.remove('open')
-    blurBackground.classList.remove('visible')
+btnXCloseMenuHam.addEventListener('click',() => {
+    // ** Cierra el menu hamburguesa removiendo la clase 'open' con el metodo toogle
+    containerMenuHam.classList.toggle('open')
+    // ** Remueve la clase visible de la clase overlay
+    blurBackground.classList.toggle('visible')
 })
 
 // ** Asegurando uso de dispositivos mobiles
 
-openMenuHam.addEventListener('touchstart', () => {
-    menuListHam.classList.toggle('open')
-    openMenuHam.classList.toggle('close')
-    blurBackground.classList.toggle('visible')
-})
+// btnOpenMenuHam.addEventListener('touchstart', () => {
+//     containerMenuHam.classList.toggle('open')
+//     btnOpenMenuHam.classList.toggle('close')
+//     blurBackground.classList.toggle('visible')
+// })
 
-closeMenuHam.addEventListener('touchstart',() => {
-    openMenuHam.classList.remove('close')
-    menuListHam.classList.remove('open')
-    blurBackground.classList.remove('visible')
-})
+// // btnXCloseMenuHam.addEventListener('touchstart',() => {
+// //     btnOpenMenuHam.classList.remove('close')
+// //     containerMenuHam.classList.remove('open')
+// //     blurBackground.classList.remove('visible')
+// // })
 
-//** Al presionar el menu hamburguesa cerrar el menu */
+// //** Al presionar el menu hamburguesa cerrar el menu */
 
-anyLinkMenuHamSelected.forEach(link => {
-    link.addEventListener('click', () => {
-        menuListHam.classList.toggle('open')
-        blurBackground.classList.toggle('visible')
-        openMenuHam.classList.toggle('open')
-    })
-})
 
-anyLinkMenuHamSelected.forEach(link => {
-    link.addEventListener('touchstart', () => {
-        menuListHam.classList.toggle('open')
-        blurBackground.classList.toggle('visible')
-        openMenuHam.classList.toggle('open')
-    })
-})
+// anyLinkMenuHamSelected.forEach(link => {
+//     link.addEventListener('touchstart', () => {
+//         console.log("entro foreach touchstart")
+//         containerMenuHam.classList.toggle('open')
+//         blurBackground.classList.toggle('visible')
+//         btnOpenMenuHam.classList.toggle('open')
+//     })
+// })
+
+// anyLinkMenuHamSelected.forEach(link => {
+//     link.addEventListener('click', () => {
+//         console.log("entro foreach click")
+//         containerMenuHam.classList.toggle('open')
+//         blurBackground.classList.toggle('visible')
+//         btnOpenMenuHam.classList.toggle('open')
+//     })
+// })
+
+// anyLinkMenuHamSelected.forEach(link => {
+//     link.addEventListener('click', () => {
+//         console.log("entro foreach click")
+//         containerMenuHam.classList.remove('open')
+//         blurBackground.classList.remove('visible')
+//         btnOpenMenuHam.classList.remove('open')
+//     })
+// })
